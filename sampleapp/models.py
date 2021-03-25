b67b92a6fa9d19491ff2105c96d6bf3a392ae20c
ogawaryoma <ookawa14106@gmail.com>
2021-03-26 02:11:38 +0900

    commit#1

---
+++ sampleapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Task(models.Model):
+  name = models.CharField('タスク名', max_length=255)
+  created_date = models.DateTimeField('作成日時', auto_now_add=True)
+  update_date = models.DateTimeField('更新日時', auto_now=True)
+
+  def __str__(self):
+    return self.name
